ee73f33c61dc2b70baf2e5918b94f8f667de4da3
user <gfgyd4@gk.llo>
2014-04-22 14:14:10 +0400

    Функция step

--- step.cpp
+++ step.cpp
@@ -1,5 +1,12 @@
-bool step(int mas[],int prov)
+#include"step.h"
+#include<conio.h>
+//#include<stdio.h>
+int step(int mas[],int prov)
 {
-    return false;
-
+    char null='0';
+    char symbol=getch();
+    int location=symbol-null;
+    if(mas[location-1]!=-1) prov=-1;
+    else prov=location;
+    return prov;
 }
--- step.h
+++ step.h
@@ -1,6 +1,6 @@
 #ifndef STEP_H
 #define STEP_H
 
-bool step(int mas[],int prov);
+int step(int mas[],int prov);
 
 #endif // STEP_H
