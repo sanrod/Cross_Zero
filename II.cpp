d38f13c322b272b556d0a3a9108d699921879260
Nikolay-Sheluhin <kolyan.fk-zenit@yandex.ru>
2014-05-01 13:46:33 +0300

    Еще не работает

--- II.cpp
+++ II.cpp
@@ -1,4 +1,20 @@
+#include<stdlib.h>
 int II(int mas[])
 {
+    srand(time(NULL));
+
+    //rand()%50;
+    int Kol-voHodov=0;
+    for(int i=0; i<10; i++)
+    {
+        if(mas[i]!=-1)
+            Kol-voHodov++;
+    }
+    switch(Kol-voHodov)
+    {
+    case 0:   int RandChislo=rand()%9;
+              mas[RandChislo]=0; break;
+    case 1:   if(mas[0]) ;
+    }
 
 }
